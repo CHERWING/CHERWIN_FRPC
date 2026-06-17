024d964988691a2ec463f737560aed96994e6fac
CHERWIN <cherwin@example.com>
2026-06-17 17:56:39 +0800

    chore: remove AdGuardHome reference, add gitignore

--- AdGuardHomeForRoot_arm64/service.sh
+++
@@ -1,7 +0,0 @@
-until [ $(getprop init.svc.bootanim) = "stopped" ]; do
-  sleep 12
-done
-
-/data/adb/agh/scripts/tool.sh start
-
-inotifyd /data/adb/agh/scripts/inotify.sh /data/adb/modules/AdGuardHome:d,n &
